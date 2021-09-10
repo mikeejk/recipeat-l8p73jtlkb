066125f8c54f663fc6ba7d76897e399bf0204669
Raja Murugan Thirumaran <rajamurugan@goschoolerp.com>
2021-09-10 11:03:10 +0530

    main

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -22,8 +22,11 @@ class CategoryController extends Controller
             'category' => 'required',
         ]);
 
+        // Send the use inputed data to create function
         Category::create($data);
 
+        // If the user click the (SAVE) button run the if condition
+        // If the user click the (SAVE AND INSERT NEXT) button run the else condition
         if ($request->get('action') == 'category_save') {
             return redirect('/admin_recipe');
         } elseif ($request->get('action') == 'category_save_next') {
--- resources/views/screens/admin/recipe/add_category.blade.php
+++ resources/views/screens/admin/recipe/add_category.blade.php
@@ -61,10 +61,12 @@
                                 <a href="/admin_recipe"><button type="button"
                                         class="btn btn-secondary mr-2 w-lg-auto w-100">Get Back</button></a>
                             </div>
+                            <!-- If Condition -->
                             <div class="col-lg-2 col-5">
                                 <button type="submit" name="action" value="category_save"
                                     class="btn btn-primary w-lg-100 w-100">Save</button>
                             </div>
+                            <!-- Else Condition -->
                             <div class="col-lg-2 col-7">
                                 <button type="submit" name="action" value="category_save_next"
                                     class="btn btn-primary w-lg-auto w-100">Save & Insert Next</button>
