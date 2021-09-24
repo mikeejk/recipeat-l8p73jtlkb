c22a9f03e2cf766b0250531008bf151535e0da43
alagu-raja <alaguraja@goschoolerp.com>
2021-09-24 20:14:39 +0530

    main

--- app/Http/Livewire/ChefQuestion.php
+++ app/Http/Livewire/ChefQuestion.php
@@ -1,13 +1,232 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Chef_question;
 use Livewire\Component;
+
 
 class ChefQuestion extends Component
 {
+    public $currentStep = 1;
+    public $name;
+    public $gender;
+    public $mail;
+    public $cookinglevel;
+    public $allergies;
+    public $lifestyle;
+    public $ingredient;
+    public $pref_cuisine;
+    public $goals;
+    public $serving_time;
+    public $cho_cook;
+    public $fav_ingr;
+    public $level_spici;
+    public $time_spend;
+
+    // Function - Render
     public function render()
     {
+        // rend the view file
         return view('livewire.chef-question');
+    }
+
+    // Function - FirstStepSubmit
+    public function firstStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'name' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 2;
+    }
+
+    // Function - SecondStepSubmit
+    public function secondStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'gender' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 3;
+    }
+
+    // Function - ThirdStepSubmit
+    public function thirdStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'mail' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 4;
+    }
+
+    // Function - FourthStepSubmit
+    public function fourthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'cookinglevel' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 5;
+    }
+
+    // Function - FifthStepSubmit
+    public function fifthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'allergies' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 6;
+    }
+
+    // Function - SixthStepSubmit
+    public function sixthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'lifestyle' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 7;
+    }
+
+    // Function - SeventhStepSubmit
+    public function seventhStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'ingredient' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 8;
+    }
+
+    // Function - EighthStepSubmit
+    public function eighthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'pref_cuisine' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 9;
+    }
+
+    // Function - NinthStepSubmit
+    public function ninthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'goals' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 10;
+    }
+
+    // Function - TenthStepSubmit
+    public function tenthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'serving_time' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 11;
+    }
+
+    // Function - EleventhStepSubmit
+    public function eleventhStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'cho_cook' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 12;
+    }
+
+    // Function - TwelfthStepSubmit
+    public function twelfthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'fav_ingr' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 13;
+    }
+
+    // Function - ThirteenthStepSubmit
+    public function thirteenthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'level_spici' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 14;
+    }
+
+    // Function - FourteenthStepSubmit
+    public function fourteenthStepSubmit()
+    {
+        // Data - Save
+        $validatedData = $this->validate([
+            'time_spend' => 'required',
+        ]);
+
+        // Next Step
+        $this->currentStep = 15;
+    }
+
+    // Function - SubmitForm
+    public function submitForm()
+    {
+        Chef_question::create([
+            'name' => $this->name,
+            'gender' => $this->gender,
+            'mail' => $this->mail,
+            'cookinglevel' => $this->cookinglevel,
+            'allergies' => $this->allergies,
+            'lifestyle' => $this->lifestyle,
+            'ingredient' => $this->ingredient,
+            'pref_cuisine' => $this->pref_cuisine,
+            'goals' => $this->goals,
+            'serving_time' => $this->serving_time,
+            'cho_cook' => $this->cho_cook,
+            'fav_ingr' => $this->fav_ingr,
+            'level_spici' => $this->level_spici,
+            'time_spend' => $this->time_spend,
+            // 'user_id' => $this->user_id,
+        ]);
+
+        // Next Step
+        $this->currentStep = 15;
+    }
+
+    // Function - Back
+    public function back($step)
+    {
+        // Back Step
+        $this->currentStep = $step;
     }
 }
