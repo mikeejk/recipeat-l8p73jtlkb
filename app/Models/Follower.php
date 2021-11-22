aacb336d509cfbd1a7e11705291a303f4bb94039
alagu-raja <alaguraja@goschoolerp.com>
2021-11-22 12:03:20 +0530

    main

--- app/Models/Follower.php
+++ app/Models/Follower.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
 
 class Follower extends Model
 {
@@ -18,5 +20,10 @@ class Follower extends Model
     public function user(){
         return $this->hasMany(User::class,'follower_id','leader_id');
     }
-    
+    // Function - Role
+    public function role()
+    {
+        // Return (role model has many data refer from the the role model)
+        return $this->hasMany(Role::class, 'role_id');
+    }
 }
