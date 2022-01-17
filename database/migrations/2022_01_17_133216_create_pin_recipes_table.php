94641b841971a054f80957aae55f6b18104b769c
alagu-raja <alaguraja@goschoolerp.com>
2022-01-17 19:04:26 +0530

    updates

---
+++ database/migrations/2022_01_17_133216_create_pin_recipes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePinRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pin_recipes', function (Blueprint $table) {
+            $table->id();
+
+        // Foreign Key - Data Saving
+        $table->unsignedBigInteger('user_id');
+        $table->unsignedBigInteger('recipe_id');
+        $table->unsignedBigInteger('pinboard_id');
+
+        //Foreign Key
+        $table->foreign('user_id')->references('id')->on('users');
+        $table->foreign('recipe_id')->references('id')->on('recipes');
+        $table->foreign('pinboard_id')->references('id')->on('pinboards');
+
+        $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pin_recipes');
+    }
+}
