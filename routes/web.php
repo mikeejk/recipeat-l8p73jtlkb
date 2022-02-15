3a7a74479460913fb9350549f4dcafb0e9167d18
alagu-raja <alaguraja@goschoolerp.com>
2022-02-15 12:30:43 +0530

    main

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Livewire\ChefQuestion;
 use App\Actions\Fortify\CreateNewUser;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FollowController;
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProfileImageController;
 use App\Models\User;
 use App\Models\Recipe;
 use Illuminate\Support\Facades\Request;
@@ -144,6 +144,12 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::middleware(['auth:sanctum', 'verified'])->get('/allrecipe.data', [RecipeController::class, 'anyData2']);
 
     Route::middleware(['auth:sanctum', 'verified'])->get('/all_recipes', [RecipeController::class, 'getIndex2']);
+
+    //     Route::middleware(['auth:sanctum', 'verified'])->get('/add_feed', [RecipeController::class, 'index3']);
+
+    //     Route::middleware(['auth:sanctum', 'verified'])->get('/all.data', [RecipeController::class, 'anyData3']);
+
+    //     Route::middleware(['auth:sanctum', 'verified'])->get('/add_feed', [RecipeController::class, 'getIndex3']);
 });
 
 // CategoryController
@@ -266,6 +272,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/update_password', functio
 Route::middleware(['auth:sanctum', 'verified'])->get('/my_profile', [Questionnaire::class, 'show']);
 
 
+
+
+
 // Edit User Profile Tab
 Route::middleware(['auth:sanctum', 'verified'])->get('/edit', [Questionnaire::class, 'edit']);
 
@@ -276,10 +285,10 @@ Route::middleware(['auth:sanctum', 'verified'])->put('/update', [Questionnaire::
 Route::middleware(['auth:sanctum', 'verified'])->get('/my_portfolio', [ChefQuestion::class, 'show']);
 
 // Edit Portfolio tab
-Route::middleware(['auth:sanctum', 'verified'])->get('/portfolio_edit', [ChefQuestion::class, 'edit']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/edit_portfolio', [ChefQuestion::class, 'edit']);
 
 // Update Portfolio tab
-Route::middleware(['auth:sanctum', 'verified'])->put('/update_portfolio', [ChefQuestion::class, 'update']);
+Route::middleware(['auth:sanctum', 'verified'])->post('/update_portfolio', [ChefQuestion::class, 'update'])->name('chef_questions.update');
 
 // -------------------------------------------------------------------------------------------------------------------
 //                                                     User-Recipe Routes
@@ -312,18 +321,19 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/recipes', [RecipeControll
 // -------------------------------------------------------------------------------------------------------------------
 //                                                     Question Routes
 // -------------------------------------------------------------------------------------------------------------------
+
 // User Questionnaire Tab
 Route::middleware(['auth:sanctum', 'verified'])->get('home', function () {
     if (auth()->user()->hasRole('Admin')) {
         return view('dashboard');
     } else {
-        return view('screens.user.home.questions');
+        return view('indexHome');
     }
 })->name('home');
 
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/indexHome', function () {
-    return view('indexHome');
+Route::middleware(['auth:sanctum', 'verified'])->get('/indexHomequestions', function () {
+    return view('screens.user.home.questions');
 });
 
 // -------------------------------------------------------------------------------------------------------------------
@@ -370,15 +380,13 @@ Route::get('/notifications', function () {
 Route::get('/contactUs', function () {
     return view('contactUs');
 });
-Route::get('/contactUs', [ContactController::class, 'contactUs']);
-Route::post('/contactUs', [ContactController::class, 'store']);
-// -------------------------------------------------------------------------
+// -------------------------------------------------------------
 //                              Test Routes
-// ------------------------------------------------------------------------
+// ------------------------------------------------------------
 Route::middleware(['auth:sanctum', 'verified'])->get('/login2', function () {
     return view('screens.user.profile.login2');
 });
-//-------------------------------------------------------------------------------
+//---------------------------------------------------------------------------
 //                               Comment Routes
 //-------------------------------------------------------------------------------
 Route::middleware(['auth:sanctum', 'verified'])->post('/comment/store', [CommentController::class, 'store'])->name('comment.add');
@@ -387,6 +395,24 @@ Route::middleware(['auth:sanctum', 'verified'])->post('/reply/store', [CommentCo
 //-----------------------------------------------------------------------------------------------
 //                                Like Routes
 //--------------------------------------------------------------------------------------------------
+//  Route::get('/', function (Request $request) {
+//      if (User::count() < 3) {
+//          User::factory()->count(3)->create();
+//     }
+
+//      if (auth()->guest() && $request->has('login')) {
+//          auth()->login(User::inRandomOrder()->first());
+//      }
+
+//      if (auth()->check() && $request->has('logout')) {
+//          auth()->logout();
+//      }
+
+//     // if (Recipe::count() < 5) {
+//     //     Recipe::factory()->count(5)->create();
+//     // }
+
+//      $recipes = Recipe::latest()->with('likes')->take(5)->get();
 
 Route::middleware(['auth:sanctum', 'verified'])->post('like', [LikeController::class, 'like'])->name('like');
 Route::middleware(['auth:sanctum', 'verified'])->delete('like', [LikeController::class, 'unlike'])->name('unlike');
@@ -405,7 +431,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     // Edit Cuisine Tab
     Route::middleware(['auth:sanctum', 'verified'])->get('/pins/{pin}/edit', [PinboardController::class, 'edit']);
-// Update Category Tab
+    // Update Category Tab
     Route::middleware(['auth:sanctum', 'verified'])->patch('/pins/{pin}', [PinboardController::class, 'update']);
     // delete Cuisine Tab
     Route::middleware(['auth:sanctum', 'verified'])->get('/pins/{pin}/delete', [PinboardController::class, 'destroy']);
@@ -431,13 +457,9 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     // Index pinrecipe Tab
     Route::middleware(['auth:sanctum', 'verified'])->get('/myfavourite', [PinRecipeController::class, 'index']);
     // Store Category Tab
-     Route::middleware(['auth:sanctum', 'verified'])->post('/recipe_view/{recipe}', [PinRecipeController::class, 'store']);
-    // Route::middleware(['auth:sanctum', 'verified'])->post('/recipe_view/{recipe}', [PinRecipeController::class, 'favouriteOrUnfavourite']);
-    // Edit Category Tab
-    Route::middleware(['auth:sanctum', 'verified'])->get('/recipe_view/{recipe_pin}', [PinRecipeController::class, 'view']);
-    // Destroy Category Tab
-    Route::middleware(['auth:sanctum', 'verified'])->get('/recipe_pin/{recipe_pin}/delete', [PinRecipeController::class, 'destroy']);
-    // Recipeat Category Data Table - Data Tab
+    Route::middleware(['auth:sanctum', 'verified'])->post('/recipe_view/{recipe}', [PinRecipeController::class, 'store']);
+
+    // cipeat Category Data Table - Data Tab
     Route::middleware(['auth:sanctum', 'verified'])->get('/recipe_pin.data', [PinRecipeController::class, 'anyData']);
     // Recipeat Category Data Table - Index Tab
     Route::middleware(['auth:sanctum', 'verified'])->get('/myfavourite', [PinRecipeController::class, 'getIndex']);
@@ -477,7 +499,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/add_feed', function () {
     return view('screens.admin.newsfeed.add_feed');
 });
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-
     Route::middleware(['auth:sanctum', 'verified'])->post('/add_feed', [FeedController::class, 'confirm']);
 
     Route::middleware(['auth:sanctum', 'verified'])->get('/add_feed', [FeedController::class, 'index']);
