a2a3ec62c9859efe1e38d2d67e1af83f42126810
Rajamurugan Thirumaran <69674346+raja-murugan@users.noreply.github.com>
2022-04-30 20:58:44 +0530

    Update Recipe_Ingredient.php

--- app/Models/Recipe_Ingredient.php
+++ app/Models/Recipe_Ingredient.php
@@ -11,7 +11,7 @@ class Recipe_Ingredient extends Model
     protected $fillable = [
         // Foreign Keys - Data Saving
         'recipe_id',
-        'ingredient_id',
+        'ingredient',
         'measurement_id',
         // User Entered - Data Saving
         'quantity',
