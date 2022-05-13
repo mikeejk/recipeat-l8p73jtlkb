b48f7e174955ec0785585db03ecda90791b29dc2
Alaguraja R <alaguraja@goschoolerp.com>
2022-05-13 17:18:22 +0530

    main

--- resources/views/mainDashboard.blade.php
+++ resources/views/mainDashboard.blade.php
@@ -7,8 +7,6 @@
 
     <title>Recipeat</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.3.4/dist/flowbite.min.css" />
